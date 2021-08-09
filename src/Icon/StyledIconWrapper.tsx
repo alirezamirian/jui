fdb4e03864f0e1ebee3283e594727b74caa3b217
Alireza <alireza.mirian@gmail.com>
2021-08-09 20:08:29 +0200

    feat(Icon): make icons inherit the current color

--- src/Icon/StyledIconWrapper.tsx
+++ src/Icon/StyledIconWrapper.tsx
@@ -10,4 +10,12 @@ export const StyledIconWrapper = styled.span<{ size?: IconSize }>`
   line-height: 0;
   width: ${({ size = DEFAULT_ICON_SIZE }) => `${size}px`};
   height: ${({ size = DEFAULT_ICON_SIZE }) => `${size}px`};
+
+  // Perhaps not so scalable to blindly change fill like this, but they have hard coded colors that should be
+  // changed to currentColor. Will look for a better approach if it started to fail for some.
+  svg rect,
+  svg path,
+  svg polygon {
+    fill: currentColor;
+  }
 `;
