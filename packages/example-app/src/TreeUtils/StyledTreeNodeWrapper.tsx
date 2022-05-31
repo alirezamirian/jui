d7440ad1a131aec081a0e1bfcdb925da2e57cc21
Alireza <alireza.mirian@gmail.com>
2022-05-31 19:38:19 +0200

    improvement(example): minor improvements

--- packages/example-app/src/TreeUtils/StyledTreeNodeWrapper.tsx
+++ packages/example-app/src/TreeUtils/StyledTreeNodeWrapper.tsx
@@ -3,4 +3,5 @@ import { styled } from "@intellij-platform/core";
 export const StyledTreeNodeWrapper = styled.div`
   display: flex;
   align-items: center;
+  padding-right: 0.25rem;
 `;
--- packages/example-app/src/VersionControl/Changes/ChangesView/ChangesViewPane.tsx
+++ packages/example-app/src/VersionControl/Changes/ChangesView/ChangesViewPane.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { ChangesViewSplitter } from "./ChangesViewSplitter";
+import { useChangeListManager } from "../change-lists.state";
 
 const StyledContainer = styled.div`
   display: flex;
@@ -9,6 +10,10 @@ const StyledContainer = styled.div`
 `;
 
 export const ChangesViewPane = () => {
+  const { refresh } = useChangeListManager();
+  useEffect(() => {
+    refresh();
+  }, []);
   return (
     <StyledContainer>
       <ChangesViewSplitter />
