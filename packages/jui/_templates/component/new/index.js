8305cd6c52f7da5099f0a837cc9223205f058fdf
Alireza <alireza.mirian@gmail.com>
2022-08-06 14:58:32 +0200

    chore: improve component generation script and template

--- packages/jui/_templates/component/new/index.js
+++ packages/jui/_templates/component/new/index.js
@@ -1,9 +1,18 @@
-// change-case is a transitive dependency, but it should be fine. Inside the template also the same version provided
+// change-case is a phantom dependency, but it should be fine. Inside the template also the same version provided
 // by hygen will be used
 const changeCase = require("change-case");
 
 module.exports = {
   params: ({ args }) => {
+    if (!args.name) {
+      const cmd =
+        process.env.npm_lifecycle_event === "generate:component"
+          ? "generate:component"
+          : "hygen component new";
+      throw new Error(
+        `No name provided. The right format is: yarn ${cmd} MyNewComponent`
+      );
+    }
     return { componentName: changeCase.pascal(args.name) };
   },
 };
