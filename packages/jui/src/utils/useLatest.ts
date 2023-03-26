9a67b5366368a99cec8bee9d3c317af1fc3f54c5
Alireza <alireza.mirian@gmail.com>
2023-03-26 23:00:11 +0200

    refactor: make useLatest hook concurrent-compatible
    refs #35

--- packages/jui/src/utils/useLatest.ts
+++ packages/jui/src/utils/useLatest.ts
@@ -1,9 +1,9 @@
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 export function useLatest<T>(value: T) {
   const ref = useRef(value);
-  // FIXME: With concurrent features enabled, it's not safe to update ref value at render phase.
-  //  Should be safe to update the value in a layoutEffect instead, but usages should be checked after the refactoring
-  ref.current = value;
+  useLayoutEffect(() => {
+    ref.current = value;
+  });
   return ref;
 }
