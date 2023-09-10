6c49588794d00bae45ec2fb68463710cbb5c3a80
Alireza <alireza.mirian@gmail.com>
2023-09-10 21:30:42 +0200

    fix(actions): fix a regression in the default action group

--- packages/jui/src/ActionSystem/components/useCreateDefaultActionGroup.tsx
+++ packages/jui/src/ActionSystem/components/useCreateDefaultActionGroup.tsx
@@ -22,42 +22,44 @@ export const useCreateDefaultActionGroup = () => {
       context: ActionContext
     ) => {
       show(({ close }) => (
-        <Popup.Layout
-          content={
-            /**
-             * NOTE: It would be much nicer to use {@link ActionGroupMenu} component here. But the action group is not
-             * yet provided when the group definition is being created. It seems like a sign of bad design that needs
-             * to get addressed.
-             */
-            <SpeedSearchMenu
-              aria-label={title}
-              items={children}
-              onAction={(key) => {
-                // The need for calculating `allActions` is a consequence of the issue explained in the note above.
-                const allActions = flatten(
-                  children.map((item) =>
-                    isActionGroupDefinition(item) ? item.children : item
-                  )
-                );
-                const action = allActions.find((action) => action.id === key);
-                if (action && !action.isDisabled) {
-                  action.actionPerformed(context);
+        <Popup>
+          <Popup.Layout
+            content={
+              /**
+               * NOTE: It would be much nicer to use {@link ActionGroupMenu} component here. But the action group is not
+               * yet provided when the group definition is being created. It seems like a sign of bad design that needs
+               * to get addressed.
+               */
+              <SpeedSearchMenu
+                aria-label={title}
+                items={children}
+                onAction={(key) => {
+                  // The need for calculating `allActions` is a consequence of the issue explained in the note above.
+                  const allActions = flatten(
+                    children.map((item) =>
+                      isActionGroupDefinition(item) ? item.children : item
+                    )
+                  );
+                  const action = allActions.find((action) => action.id === key);
+                  if (action && !action.isDisabled) {
+                    action.actionPerformed(context);
+                  }
+                }}
+                onClose={close}
+                autoFocus="first"
+              >
+                {(item) =>
+                  renderActionAsMenuItem({
+                    ...item,
+                    // a consequence of the issue explained in the note above.
+                    shortcut: getActionShortcut(item.id),
+                  })
                 }
-              }}
-              onClose={close}
-              autoFocus="first"
-            >
-              {(item) =>
-                renderActionAsMenuItem({
-                  ...item,
-                  // a consequence of the issue explained in the note above.
-                  shortcut: getActionShortcut(item.id),
-                })
-              }
-            </SpeedSearchMenu>
-          }
-          header={title}
-        />
+              </SpeedSearchMenu>
+            }
+            header={title}
+          />
+        </Popup>
       ));
     }
   );
