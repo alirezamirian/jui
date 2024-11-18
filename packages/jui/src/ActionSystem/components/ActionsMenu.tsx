dbbe0b3de09dd4be819eff5d0c31fe81aa70e69d
Alireza <alireza.mirian@gmail.com>
2024-11-18 21:27:58 +0100

    fix(ActionsMenu): fix props menu props not passed to Menu

--- packages/jui/src/ActionSystem/components/ActionsMenu.tsx
+++ packages/jui/src/ActionSystem/components/ActionsMenu.tsx
@@ -55,7 +55,7 @@ export type ActionMenuProps = {
 export function ActionsMenu({
   actions,
   actionContext,
-  children = (actionMenuProps) => <Menu {...otherProps} {...actionMenuProps} />,
+  children = (actionMenuProps) => <Menu {...actionMenuProps} />,
   ...otherProps
 }: ActionMenuProps) {
   const allActions = getAllActions(actions);
@@ -66,6 +66,7 @@ export function ActionsMenu({
   return (
     <>
       {children({
+        ...otherProps,
         onAction: (key) => {
           const action = allActions.find(({ id }) => id === key);
           if (action && isAction(action)) {
